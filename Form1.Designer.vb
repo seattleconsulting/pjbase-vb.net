90f2af47684c986500d4d47967fe1546a507812d
MiMiKo <scm.mimiko@gmail.com>
2020-12-22 12:38:41 +0900

    ぽプロジェクトファイル追加

---
+++ Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class sampleform
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.rdone = New System.Windows.Forms.RadioButton()
+        Me.rdtwo = New System.Windows.Forms.RadioButton()
+        Me.getdata = New System.Windows.Forms.Button()
+        Me.insert = New System.Windows.Forms.Button()
+        Me.update = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.delete = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(797, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(526, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CRUDチュートリアルフォーム(VB.Net)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(86, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "身分証"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Location = New System.Drawing.Point(86, 242)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "住所"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label4.Location = New System.Drawing.Point(86, 182)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "名前"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label5.Location = New System.Drawing.Point(86, 305)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 18)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "県"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label6.Location = New System.Drawing.Point(86, 368)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 18)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "年齢"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label7.Location = New System.Drawing.Point(86, 424)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(46, 18)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "性別"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(212, 123)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(284, 27)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(212, 368)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(284, 27)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(212, 242)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(284, 27)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(212, 179)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(284, 27)
+        Me.TextBox2.TabIndex = 11
+        '
+        'rdone
+        '
+        Me.rdone.AutoSize = True
+        Me.rdone.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.rdone.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.rdone.Location = New System.Drawing.Point(212, 438)
+        Me.rdone.Name = "rdone"
+        Me.rdone.Size = New System.Drawing.Size(55, 24)
+        Me.rdone.TabIndex = 13
+        Me.rdone.TabStop = True
+        Me.rdone.Text = "男"
+        Me.rdone.UseVisualStyleBackColor = True
+        '
+        'rdtwo
+        '
+        Me.rdtwo.AutoSize = True
+        Me.rdtwo.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.rdtwo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.rdtwo.Location = New System.Drawing.Point(428, 438)
+        Me.rdtwo.Name = "rdtwo"
+        Me.rdtwo.Size = New System.Drawing.Size(55, 24)
+        Me.rdtwo.TabIndex = 14
+        Me.rdtwo.TabStop = True
+        Me.rdtwo.Text = "女"
+        Me.rdtwo.UseVisualStyleBackColor = True
+        '
+        'getdata
+        '
+        Me.getdata.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.getdata.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.getdata.Location = New System.Drawing.Point(523, 123)
+        Me.getdata.Name = "getdata"
+        Me.getdata.Size = New System.Drawing.Size(186, 40)
+        Me.getdata.TabIndex = 16
+        Me.getdata.Text = "取得"
+        Me.getdata.UseVisualStyleBackColor = True
+        '
+        'insert
+        '
+        Me.insert.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.insert.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.insert.Location = New System.Drawing.Point(523, 179)
+        Me.insert.Name = "insert"
+        Me.insert.Size = New System.Drawing.Size(186, 40)
+        Me.insert.TabIndex = 17
+        Me.insert.Text = "登録"
+        Me.insert.UseVisualStyleBackColor = True
+        '
+        'update
+        '
+        Me.update.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.update.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.update.Location = New System.Drawing.Point(523, 242)
+        Me.update.Name = "update"
+        Me.update.Size = New System.Drawing.Size(186, 40)
+        Me.update.TabIndex = 18
+        Me.update.Text = "編集"
+        Me.update.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(803, 123)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(1073, 417)
+        Me.DataGridView1.TabIndex = 19
+        '
+        'clear
+        '
+        Me.clear.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.clear.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.clear.Location = New System.Drawing.Point(523, 302)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(186, 40)
+        Me.clear.TabIndex = 21
+        Me.clear.Text = "クリア"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'delete
+        '
+        Me.delete.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.delete.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.delete.Location = New System.Drawing.Point(523, 368)
+        Me.delete.Name = "delete"
+        Me.delete.Size = New System.Drawing.Size(186, 40)
+        Me.delete.TabIndex = 22
+        Me.delete.Text = "削除"
+        Me.delete.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(212, 316)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(284, 26)
+        Me.ComboBox1.TabIndex = 23
+        '
+        'sampleform
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1581, 600)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.delete)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.update)
+        Me.Controls.Add(Me.insert)
+        Me.Controls.Add(Me.getdata)
+        Me.Controls.Add(Me.rdtwo)
+        Me.Controls.Add(Me.rdone)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "sampleform"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents rdone As System.Windows.Forms.RadioButton
+    Friend WithEvents rdtwo As System.Windows.Forms.RadioButton
+    Friend WithEvents getdata As System.Windows.Forms.Button
+    Friend WithEvents insert As System.Windows.Forms.Button
+    Friend WithEvents update As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents clear As System.Windows.Forms.Button
+    Friend WithEvents delete As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
